e6ddd106bc478626668310e1fa18f3415d823eaf
Sheldon Stevens <sgste080@yahoo.com>
2021-06-18 18:03:23 -0700

    ignored

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql');
-
+const mysql = require("mysql");
 
 connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'employee_db',
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "employee_db",
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    console.log(`Connected to db. ThreadID: ${connection.threadId}`);
-})
+  console.log(`Connected to db. ThreadID: ${connection.threadId}`);
+});
 
 module.exports = connection;
